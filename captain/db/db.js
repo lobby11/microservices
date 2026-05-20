757eb6436099251559cb086ad5be09c24040b852
lobby11 <nitiniiitr@gmail.com>
2026-05-20 14:43:49 +0530

    captain service created and conencted to gateway

---
+++ captain/db/db.js
@@ -0,0 +1,9 @@
+ const mongoose = require('mongoose');
+ function connect(){
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("captain service connected to database");
+    }).catch((err)=>{
+console.log(err)
+    })
+}
+module.exports = {connect};
---
+++ captain/routes/captain.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const captainController = require('../controllers/captain.controller');
+const authMiddleware = require('../middleware/authMiddleWare');
+router.post('/register', captainController.register);
+router.post('/login', captainController.login);
+
+router.get('/logout',  captainController.logout);
+router.get('/profile', authMiddleware.captainAuth, captainController.profile);
+router.patch('/updateAvailability', authMiddleware.captainAuth, captainController.updateAvailability);
+
+module.exports = router;

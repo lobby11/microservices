c78df436cb918ed76564de7287ced19294b7c4ef
lobby11 <nitiniiitr@gmail.com>
2026-05-20 12:12:33 +0530

    first commit

---
+++ user/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser');
+const connect = require('./db/db').connect
+connect()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/', userRoutes);
+
+module.exports = app;
---
+++ user/db/db.js
@@ -0,0 +1,9 @@
+ const mongoose = require('mongoose');
+ function connect(){
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("user service connected to database");
+    }).catch((err)=>{
+console.log(err)
+    })
+}
+module.exports = {connect};
